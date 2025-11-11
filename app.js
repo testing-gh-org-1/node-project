64724d00d0a484ae0536c418d5d0c89ab3706bfb
thribhuvant <thribhuvana.t@digicert.com>
2025-11-11 13:15:12 +0530

    added app

---
+++ app.js
@@ -0,0 +1,11 @@
+const lodash = require('lodash');
+const prismjs = require('prismjs');
+const redux = require('redux');
+const fetch = require('node-fetch');
+
+
+console.log('Lodash version:', lodash.VERSION);
+console.log('PrismJS version:', prismjs.languages);
+console.log('Redux version:', redux.VERSION);
+console.log('node-fetch version:', fetch.version);
+console.log('App is running...');
